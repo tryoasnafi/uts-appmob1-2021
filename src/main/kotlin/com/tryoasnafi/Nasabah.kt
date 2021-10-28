18ed4dd3e91b827bb21ed0e68295e27dc7225929
Tryo Asnafi <tryoasnafi@gmail.com>
2021-10-28 11:37:34 +0700

    Add soal 3: UML Nasabah

---
+++ src/main/kotlin/com/tryoasnafi/Nasabah.kt
@@ -0,0 +1,8 @@
+package com.tryoasnafi
+
+class Nasabah(a: Int, b: String, c: String, d: Int) {
+    val rekening = a
+    val nama = b
+    val alamat = c
+    val noKontak = d
+}
---
+++ src/main/kotlin/com/tryoasnafi/NasabahTest.kt
@@ -0,0 +1,9 @@
+package com.tryoasnafi
+
+fun main() {
+    val nasabah1 = Nasabah(109274, "Tryo Asnafi", "Jl. Lintas Timur", 628521111)
+    println("No. Rekening \t: ${nasabah1.rekening}")
+    println("Nama Nasabah \t: ${nasabah1.nama}")
+    println("Alamat Nasabah \t: ${nasabah1.alamat}")
+    println("No. Kontak \t\t: ${nasabah1.noKontak}")
+}
